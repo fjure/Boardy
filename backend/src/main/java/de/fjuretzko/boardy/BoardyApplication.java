c4c2520425c123405a65384628a25c59b97585db
Florian Juretzko <fjuretzko@gmail.com>
2023-05-18 03:43:09 +0200

    Initialized project

---
+++ backend/src/main/java/de/fjuretzko/boardy/BoardyApplication.java
@@ -0,0 +1,13 @@
+package de.fjuretzko.boardy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoardyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BoardyApplication.class, args);
+    }
+
+}
---
+++ backend/src/test/java/de/fjuretzko/boardy/BoardyApplicationTests.java
@@ -0,0 +1,13 @@
+package de.fjuretzko.boardy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoardyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
