d5b4f8cc3651c5f5bf57475766ceadb67f3f6a20
Florian Juretzko <fjuretzko@gmail.com>
2023-06-08 01:46:25 +0200

    added tailwind css to the project

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: ["./pages/**/*.{ts,tsx}", "./stories/**/*.{ts,tsx}", "./src/**/*.{js,jsx,ts,tsx}",],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+}
+
